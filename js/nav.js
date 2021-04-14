c8ebdceffafbe236703dbb7c391867cc0f77895f
Krosmez <jonathanmaison78@gmail.com>
2021-04-14 17:16:48 +0200

    JS COMMIT SLIDER

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+const toggleMenu = document.querySelector('.toggle-menu');
+const menu = document.querySelector('.menu');
+
+toggleMenu.addEventListener('click', function() {
+  const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
+  toggleMenu.setAttribute('aria-expanded', !open);
+  menu.hidden = !menu.hidden;
+  document.body.classList.toggle('no-scroll');
+});
